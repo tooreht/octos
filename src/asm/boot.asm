5a3878d9d461b2b67ba04dbd89f7d09fd58df750
tooreht <tooreht@gmail.com>
2017-03-08 23:30:05 +0100

    Tidy and comment asm files

--- src/asm/boot.asm
+++ src/asm/boot.asm
@@ -1,21 +1,23 @@
 extern kmain
 global start
 
-section .text
+section .boot
 bits 32
 start:
 
-    ; Point the first entry of the level 4 page table
+    ; point the first entry of the level 4 page table
     ; to the first entry in the p3 table.
     mov eax, p3_table
     or eax, 0b11
     mov dword [p4_table + 0], eax
-    ; Point the first entry of the level 3 page table
+
+    ; point the first entry of the level 3 page table
     ; to the first entry in the p2 table.
     mov eax, p2_table
     or eax, 0b11
     mov dword [p3_table + 0], eax
-    ; Point each page table level two entry to a page.
+
+    ; point each page table level two entry to a page
     mov ecx, 0  ; counter variable
 .map_p2_table:
     mov eax, 0x200000   ; 2MiB
@@ -27,28 +29,28 @@ start:
     cmp ecx, 512
     jne .map_p2_table
 
-    ; Move page table address to cr3
+    ; move page table address to cr3
     mov eax, p4_table
     mov cr3, eax
 
-    ; Enable PAE (Physical Address Extension)
+    ; enable PAE (Physical Address Extension)
     mov eax, cr4
     or eax, 1 << 5
     mov cr4, eax
 
-    ; Set the long mode bit
+    ; set the long mode bit
     mov ecx, 0xC0000080
     rdmsr
     or eax, 1 << 8
     wrmsr
 
-    ; Enable paging
+    ; enable paging
     mov eax, cr0
     or eax, 1 << 31
     or eax, 1 << 16
     mov cr0, eax
 
-    ; Tell the hardware about our GDT
+    ; tell the hardware about our GDT
     lgdt [gdt64.pointer] ; load global descriptor table
 
     ; update selectors
@@ -57,11 +59,14 @@ start:
     mov ds, ax
     mov es, ax
 
-    ; Jump to long mode!
+    ; long jump to kmain setting `cs` register to `gdt64.code`
     jmp gdt64.code:kmain
 
+    ; shouldn't ever happen
+    hlt
+
 section .bss ; block started by symbol
-    ; Entries in the bss section are automatically set to zero
+    ; entries in the bss section are automatically set to zero
     ; by the linker.
 
     align 4096
@@ -75,19 +80,19 @@ section .bss ; block started by symbol
 
 section .rodata
 gdt64:
-    dq 0    ; define quad-word
+    dq 0    ; zero entry - dq = define quad-word
 .code: equ $ - gdt64
     ; 44: ‘descriptor type’: This has to be 1 for code and data segments
     ; 47: ‘present’: This is set to 1 if the entry is valid
     ; 41: ‘read/write’: If this is a code segment, 1 means that it’s readable
     ; 43: ‘executable’: Set to `1 for code segments
     ; 53: ‘64-bit’: if this is a 64-bit GDT, this should be set
-    dq (1<<44) | (1<<47) | (1<<41) | (1<<43) | (1<<53)
+    dq (1<<44) | (1<<47) | (1<<41) | (1<<43) | (1<<53) ; code segment
 .data: equ $ - gdt64
     ; 44: ‘descriptor type’: This has to be 1 for code and data segments
     ; 47: ‘present’: This is set to 1 if the entry is valid
     ; 41: ‘read/write’: If this is a data segment, 1 means that it’s writable
-    dq (1<<44) | (1<<47) | (1<<41)
+    dq (1<<44) | (1<<47) | (1<<41) ; data segment
 .pointer:
     dw .pointer - gdt64 - 1
     dq gdt64
--- src/asm/multiboot_header.asm
+++ src/asm/multiboot_header.asm
@@ -1,11 +1,12 @@
 section .multiboot_header
 header_start:
-    dd 0xe85250d6                ; magic number
-    dd 0                         ; architecture (protected mode code)
+    dd 0xe85250d6                ; magic number (multiboot 2)
+    dd 0                         ; architecture 0 (protected mode i386)
     dd header_end - header_start ; header length
-
     ; checksum
     dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+    ; insert optional multiboot tags here
 
     ; required end tag
     dw 0    ; type
